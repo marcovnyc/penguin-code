e067c5b1e2716b3f783411cf05f51c9eb5812362
marcovnyc <marcovnyc@gmail.com>
2017-06-01 22:05:08 -0400

    Learning code

---
+++ python-head-first/chapter01/beer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 15:33:53 2017
+The Beer Song
+@author: marco
+"""
+
+word = "bottles"
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("You take one down and pass it around.")
+    if beer_num == 1:
+        print("No more bottles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = "bottle"
+        print(new_num, word, "of beer on the wall.")
+    print()
---
+++ python-head-first/chapter01/elefantes.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 17:19:28 2017
+The Elephant Song.
+@author: marco
+"""
+word = "elefantes"
+
+for swings in range(1, 15, +1)
