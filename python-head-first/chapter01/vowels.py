603f78a8608bac69174ea6293f2c82cde52aa112
Marco The Penguin <marcovnyc@gmail.com>
2017-06-01 23:38:27 -0400

    Learning python lists

---
+++ python-head-first/chapter01/vowels.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 23:12:51 2017
+Vowels Code
+@author: mmaldonado
+"""
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = "Milliways"
+for letter in word:
+    if letter in vowels:
+        print(letter)
---
+++ python-head-first/chapter01/vowels2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 23:12:51 2017
+Vowels Code
+@author: mmaldonado
+"""
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("Provide a word to search for vowels: ")
+found = []
+
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+for vowel in found:
+    print(vowel)            
