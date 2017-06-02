53a3bf2850e205fff5a6bff6482310c508ca9185
Marco The Penguin <marcovnyc@gmail.com>
2017-06-01 22:50:21 -0400

    Added elefants but i am still getting an error

--- python-head-first/chapter01/elefantes.py
+++ python-head-first/chapter01/elefantes.py
@@ -6,4 +6,15 @@ The Elephant Song.
 """
 word = "elefantes"
 
-for swings in range(1, 15, +1)
+for num_elephants in range(0, 15, +1):
+    print(num_elephants, word, "se columpiaban sobre")
+    print("la tela de una arana y como vieron que resistia")
+    print("Fueron a llamara otro elefante")
+    if num_elephants == 15:
+        print(num_elephants, "se columpiaban y la tela se rompio")
+    else:
+        swing_elephant = num_elephants + 1
+        if swing_elephant == 1:
+            word = "elefante"
+        print(swing_elephant, word, "se columpiaba")
+    print()
