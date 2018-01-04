b27d1892b31d41b1eae22ecf28b68bf97d7cf87f
Marco The Penguin <marcovnyc@gmail.com>
2018-01-03 23:35:55 -0500

    Automate the borin things. Ask your lord of the Rings Name

---
+++ automate-the-boring/lord_of_the_rings_name.py
@@ -0,0 +1,6 @@
+print('Hello and Welcome to Isengard')
+print('Welcome stranger what is your name?:')
+myName = input()  # Ask for name here
+print('The doors of Isengard are open to you ' + myName) # Example Aragorn 
+print('The Length of your name is the size of your sword:')
+print(len(myName))
