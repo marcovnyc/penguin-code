46a6ee67b9abc3f104fa1485eb0346d09333b9a9
Marco The Penguin <marcovnyc@gmail.com>
2018-01-04 00:22:19 -0500

    Adding last lor of the rings

---
+++ automate-the-boring/lord_of_the_rings_name-v2.py
@@ -0,0 +1,9 @@
+print('Hello and Welcome to Isengard')
+print('Welcome stranger what is your name?:')
+myName = input()  # Ask for name here
+print('The doors of Isengard are open to you ' + myName) # Example Aragorn 
+print('The Length of your name is the size of your sword:')
+print(len(myName))
+print('What is your age? ' + myName )
+myAge = input()
+print(myName + ' You will be  ' + str(int(myAge) + 1)  + ' In a year' )
