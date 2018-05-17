9158f07af515e0f0ab9a853daa78991d2f9d3013
Marco Penguin <marcovnyc@gmail.com>
2018-05-16 21:20:23 -0400

    Adding my Python project files

--- automate-it/chapter-01/github.py
+++ automate-it/chapter-01/github.py
@@ -1,4 +1,4 @@
 import requests
-payload = {'q': 'marcovnyc'}
+payload = {'q': 'chetan'} 
 r = requests.get('https://github.com/search', params=payload)
-print("Request URL:", r.response)
+print("Request URL:", r.url)
---
+++ master-class/HelloCruelWorld.py
@@ -0,0 +1,4 @@
+print('Hello Cruel World!')
+print("This is My First Python Class")
+print(2 * 3)
+print('The end')
