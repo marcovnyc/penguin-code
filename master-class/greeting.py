b7e5b4fd9a54aa6cbf089b7a691b8971ce13d04a
Marco Penguin <marcovnyc@gmail.com>
2018-05-16 23:08:58 -0400

    Adding more stuff to greeting

--- master-class/greeting.py
+++ master-class/greeting.py
@@ -2,3 +2,7 @@ __author__ = 'dev'
 greeting = "Hello, how are you today" 
 name = input("Please enter your name:")
 print(greeting + ' ' + name)
+
+
+splitString = "This string has been \nsplit over\nseveral\nlines"
+print(splitString)
