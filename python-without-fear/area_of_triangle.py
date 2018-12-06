d23acbf3ddbfffac70fc1bc4ec91d137ba819626
marcovnyc <marcovnyc@gmail.com>
2018-12-06 11:42:43 -0500

    Calculate Area of Triangle

---
+++ python-without-fear/area_of_triangle.py
@@ -0,0 +1,6 @@
+# Calculate the Area of Triangle #
+
+base = float(input('Enter base of Triangle:  '))
+height = float(input('Enter height of Triangle: '))
+area = height * base * 0.5
+print('The Area of The Triangle is', area, ' inches')
