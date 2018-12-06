bd892d9fd4e7f04cab0a00526975f441513da775
marcovnyc <marcovnyc@gmail.com>
2018-12-06 13:19:14 -0500

    Adding a BMI calculator in python

---
+++ python-without-fear/calculate_bmi.py
@@ -0,0 +1,9 @@
+ ## BMI = (	Weight in Pounds / (Height in inches) x (Height in inches)) x 703 ##
+
+h_feet = int(input('Enter your Height in Feet: '))
+h_inches =  int(input('Enter your Height inches: '))
+W = int(input('Enter your Weight in Pounds: '))
+height = h_feet * 12 + h_inches
+
+BMI =  (W / ( height * height)) * 703
+print('Your BMI is ', BMI)
