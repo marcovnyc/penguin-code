b66fe8f0a99e63982f18db57900d05d27039beb3
marcovnyc <marcovnyc@gmail.com>
2018-12-06 13:20:40 -0500

    BMI todo

--- python-without-fear/calculate_bmi.py
+++ python-without-fear/calculate_bmi.py
@@ -7,3 +7,10 @@ height = h_feet * 12 + h_inches
 
 BMI =  (W / ( height * height)) * 703
 print('Your BMI is ', BMI)
+
+# Todo: Depending on your BMI you are told you are either one of the status below 
+#BMI	Weight Status
+#Below 18.5	Underweight
+#18.5 – 24.9	Normal
+#25.0 – 29.9	Overweight
+#30.0 and Above	Obese
